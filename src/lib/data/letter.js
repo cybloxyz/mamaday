b669a831313aa99b78c5f10a6052b4c23f8270a3
cybloxyz <tyroarrr@gmail.com>
2026-05-02 15:30:43 +0700

    add mail.jpeg

--- src/lib/data/letter.js
+++ src/lib/data/letter.js
@@ -2,22 +2,22 @@
 export const secretLetters = {
   "H7X": {
     title: "Letter for our hero",
-    img: "/letters/hasanah.png",
+    img: "/mail.jpeg",
     message: "selamat hari ibu zah...terima kasih untuk waktu, tenaga dan kesabarannya selama membersamai kami, mulai dari bangun pagi hingga tidur lagi, terima kasih untuk tetap hadir disini, semoga semua lelah terbalaskan dengan bahagia dan kesuksesan di masa depan nanti <3"
   },
   "U4F": {
     title: "Letter for our hero",
-    img: "/letters/ulya.png",
+    img: "/mail.jpeg",
     message: "selamat hari ibu zah...terima kasih untuk waktu, tenaga dan kesabarannya selama mengajarkam kami di sekolah, semua ilmu yang ustazah berikan akan selalu menjadi amal jariyah bagi ustazah, ilmu dari ustazah yang kelak kami gunakan untuk menjadi sukses esok hari, semoga semua lelah terbalaskan dengan bahagia dan kesuksesan di masa depan nanti <3"
   },
   "S9Z": {
     title: "Letter for our hero",
-    img: "/letters/saadah.png",
+    img: "/mail.jpeg",
     message: "selamat hari ibu,...terima kasih sudah menjadi ibu yang baik dan kuat, terima kasih untuk tenaga dan kesabarannya, terima kasih untuk waktu yang sudah dilalui, semoga semua lelah terbalaskan dengan bahagia dan kesuksesan di masa depan nanti <3"
   },
   "A2B": {
     title: "Letter for our hero",
-    img: "/letters/izzah.png",
+    img: "mail.jpeg",
     message: "selamat hari ibu zah...terima kasih untuk semua kontribusinya zah, untuk waktu, tenaga dan kesabarannya selama berada disini, terima kasih untuk tetap hadir, semoga semua lelah terbalaskan dengan bahagia dan kesuksesan di masa depan nanti <3"
   },
 };
